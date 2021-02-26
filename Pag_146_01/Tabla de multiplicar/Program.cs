c2073a48f417758177b76816d6272962ed562238
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-25 22:01:43 -0600

    Add files via upload

---
+++ Pag_146_01/Tabla de multiplicar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabla_de_multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0,nt, resultado;
+            Console.Write("Ingrese el numero que quieres calcular su tabla de multiplicar: ");
+            nt = int.Parse(Console.ReadLine());
+            Console.Write("Calculando la tabla del {0} ", nt);
+            while (contador <= 9)
+            {
+                contador++;
+                resultado = contador * nt;
+                Console.WriteLine("\n \n" + nt + " * " + contador + " = " + resultado);
+            }
+        }
+    }
+}
