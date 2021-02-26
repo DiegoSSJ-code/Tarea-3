e6eea362cbd4edafbf7dbfe8d69854a0ac4460de
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-25 22:06:05 -0600

    Add files via upload

---
+++ Pag_146_04/Primo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int div = 1;
+            int contador = 0;
+            for(int i = 2; i < 1001; i++)
+            {
+                contador = 0;
+                for (div = 1; div <=i; div++)
+                {
+                    if (i % div == 0)
+                    {
+                        contador++;
+                    }
+                    if (contador < 3)
+                    {
+                        Console.WriteLine("El " + i + "es un numero primo");
+                    }
+                }
+            }
+        }
+    }
+}
