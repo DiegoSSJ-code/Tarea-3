2b82705fc42e68b15e6cb5f8072105a0fa9eb5d2
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-25 22:05:17 -0600

    Add files via upload

---
+++ Pag_146_02/Potencia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, p, Pot;
+            Console.WriteLine("Ingrese el numero que desea hacer la potencia: ");
+            n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la potencia: ");
+            p = double.Parse(Console.ReadLine());
+            Pot = Math.Pow(n, p);
+            Console.WriteLine("La potencia de " + n + "a " + p + "es : " + Pot);
+        }
+    }
+}
