a22567ed4faa927e3fa2a1fa783f2dccd0bde70b
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-25 21:47:55 -0600

    Add files via upload

---
+++ Pag_110_01/Par_impar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Par_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Ingrese un numero: ");
+            n = Int32.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
