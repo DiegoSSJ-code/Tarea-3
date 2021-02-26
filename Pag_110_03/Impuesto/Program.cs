1e89827083c5b998f966b08ffb6bf5059a83531d
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-25 21:55:38 -0600

    Add files via upload

---
+++ Pag_110_03/Impuesto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Impuesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string producto;
+            double impuesto, precio;
+            Console.WriteLine("Ingrese el tipo de producto: ");
+            producto = Console.ReadLine();
+            Console.WriteLine("Ingrese el precio del producto");
+            precio = double.Parse(Console.ReadLine());
+            if (producto == "medicina")
+            {
+                impuesto = 0;
+                Console.Write("El impuesto es de: " + impuesto);
+            }
+            else
+            {
+                impuesto = precio * 0.09;
+                Console.Write("El impuesto es de: " + impuesto);
+            }
+        }
+    }
+}
+
