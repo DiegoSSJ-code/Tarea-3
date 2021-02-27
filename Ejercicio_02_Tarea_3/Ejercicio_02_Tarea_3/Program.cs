e52be7f21fb03e1be17f78c548d68e81f6cf1c39
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-27 10:47:35 -0600

    Add files via upload

---
+++ Ejercicio_02_Tarea_3/Ejercicio_02_Tarea_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_02_Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, n, i, aux;
+            Console.Write("Ingrese el número de la serie que desea mostrar: ");
+            n = int.Parse(Console.ReadLine());
+            x = 0;
+            y = 1;
+            for (i = 0; i < n; i++)
+            {
+                aux = x;
+                x = y;
+                y = aux + x;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
