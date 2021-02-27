4931fa1958a7d299051f713588ef1ae31916d5b9
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-27 10:37:27 -0600

    Add files via upload

---
+++ Ejercicio_01_Tarea_3/Ejercicio_01_Tarea_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio_01_Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Por favor Digite un número: ");
+                num = int.Parse(Console.ReadLine());
+                verif(num);
+            } while (num < 0);
+            fact(num);
+        }
+        static void verif(int n)
+        {
+            if (n < 0)
+            {
+                Console.Write("\nError: No existe el factorial de un número negativo!!!!!\n");
+            }
+        }
+        static void fact(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("\nEl Factorial de {0} es: {1}", n, fact);
+        }
+    }
+}
